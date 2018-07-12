d787fbea01c14085233f5a039f04e6b27f840135
dinobobo <kenzocyp@gmail.com>
2018-07-12 13:11:55 -0400

    simplified and added code to sort pairs

--- merge_sort.py
+++ merge_sort.py
@@ -47,4 +47,42 @@ def merge_sort(x):
                     j += 1 
     return(x)
     
+def sort_array_pairs(x,axis = 0):   
+    for j in range(len(x)):
+        min_idx = j
+        for i in range(j+1,len(x)):
+            if x[i,axis] <= x[j,axis]:
+                min_idx = i
+        x[j], x[min_idx] = x[min_idx].copy(), x[j].copy() #when assign a list to variable, use copy so it will not be changed      
+    return(x) 
+   
+def merge_sort_pairs(x, axis = 0):
+    if len(x) < 3:
+        sort_array_pairs(x,axis)
+    else:
+        L = x[0 : int(len(x)/2)].copy()
+        R = x[int(len(x)/2): len(x)].copy()
+        L = merge_sort_pairs(L)
+        R = merge_sort_pairs(R)
+        i = 0
+        j = 0
+        k = 0
+        for i in range(len(x)):
+            if j < len(L) and k <len(R):
+                if L[j,axis]<=R[k,axis]:
+                    x[i] = L[j]
+                    j += 1
+                else:
+                    x[i] = R[k]
+                    k += 1
+                   
+            else:
+                if j == len(L):
+                    x[i] = R[k]
+                    k += 1
+                else:
+                    x[i] = L[j]
+                    j += 1 
+    return(x)    
+    
 print(merge_sort([2,5,6,3,2,9,11,109,2,4,5,1]))
