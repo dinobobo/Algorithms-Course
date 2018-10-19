87b1b841abace8ce287e834c824a693b8e880e7b
dinobobo <kenzocyp@gmail.com>
2018-10-18 21:25:36 -0400

    added linearize ordering

--- DFS.py
+++ DFS.py
@@ -4,26 +4,36 @@ Created on Thu Aug  9 12:54:18 2018
 
 @author: DinoBob
 """
+def pre(s):
+    global clock
+    order[s] = [clock]
+    clock += 1
 
+def post(s):
+    global clock
+    order[s].append(clock)
+    clock += 1
+    
 def DFS(G,s):
     for i in G.keys():
-        G[i].append('False')
+        G[i].append(False)
     st = [s]
+    pre(s)
     while len(st) > 0:
-        advance = 'False'
-        G[st[-1]][-1] = 'True'
+        advance = False
+        G[st[-1]][-1] = True
         for j in G[st[-1]][:-1]:
-            if G[str(j)][-1] == 'False':
+            if G[str(j)][-1] == False:
                 st.append(str(j))
-                advance = 'True'
+                pre(str(j))
+                advance = True
                 break
-        if advance == 'False':    
-            st.pop()    
-                
-            
-            
+        if advance == False:    
+            post(str(st.pop()))    
     
     
-    
-graph = {'1':[2,3],'2':[1,4],'3':[1,4,6],'4':[2,3,5,6],'5':[4,6],'6':[3,4,5]}
-DFS(graph, '1') 
+if __name__ == '__main__':   
+    graph = {'1':[2,3],'2':[1,4],'3':[1,4,6],'4':[2,3,5,6],'5':[4,6],'6':[3,4,5]}    
+    order = {}  
+    clock = 1
+    DFS(graph, '1')    
--- DFS_recur.py
+++ DFS_recur.py
@@ -4,20 +4,35 @@ Created on Sat Aug 11 08:14:17 2018
 
 @author: DinoBob
 """
-def DFS_recur(G,s):    
-    G[s][-1] = 'True'
+def pre(s):
+    global clock
+    order[s] = [clock]
+    clock += 1
+
+def post(s):
+    global clock
+    order[s].append(clock)
+    clock += 1
+    
+def Explore_recur(G,s):   
+    G[s][-1] = True
+    pre(s)
     for j in G[s][:-1]:
-        if G[str(j)][-1] == 'False':
-            DFS_recur(G, str(j))
-        
-        
-                
-            
-            
-    
-    
-graph = {'1':[2,3],'2':[1,4],'3':[1,4,6],'4':[2,3,5,6],'5':[4,6],'6':[3,4,5]}
-for i in graph.keys():
-        graph[i].append('False')
-        
-DFS_recur(graph, '1') 
+        if G[str(j)][-1] == False:
+            Explore_recur(G, str(j))
+    post(s)
+'''      
+def DFS(G):
+    for i in G.keys():
+        G[i][-1] = 'False'
+
+    for j in G.keys():
+        if G[j][-1] == 'False':
+            Explore_recur(G, i)        
+'''            
+if __name__ == '__main__':   
+    graph = {'1':[2,3, False],'2':[1,4, False],'3':[1,4,6, False],'4':[2,3,5,6, False],'5':[4,6, False],'6':[3,4,5, False]}    
+    order = {}  
+    clock = 1
+    Explore_recur(graph, '1')    
+#    DFS(graph) 
